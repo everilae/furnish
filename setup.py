e7b2d35a2bfe77bc481330fc51c54b63b879c16a
Ilja Everilä <ilja.everila@siili.com>
2017-05-23 12:41:46 +0300

    Update dist info

--- setup.py
+++ setup.py
@@ -1,13 +1,17 @@
 from setuptools import setup
 
+with open("README.md") as f:
+    long_description = f.read()
+
 setup(
     name="furnish",
-    version="0.1",
+    version="0.1.0",
     description="Create HTTP API clients from Python.",
+    long_description=long_description,
     author="Ilja Everilä",
     author_email="saarni@gmail.com",
     url="https://github.com/everilae/furnish",
-    download_url="https://github.com/everilae/furnish/archive/v0.1.tar.gz",
+    download_url="https://github.com/everilae/furnish/archive/v0.1.0.tar.gz",
     packages=["furnish"],
     setup_requires=[ "pytest-runner" ],
     tests_require=[ "pytest" ],
