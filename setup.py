3ceb7746c6717a386596d5ccd4f054c873617584
Ilja Everilä <ilja.everila@siili.com>
2017-05-23 12:51:32 +0300

    Pump version, for pypi formatting

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ with open("README.rst") as f:
 
 setup(
     name="furnish",
-    version="0.1.0",
+    version="0.1.1",
     description="Create HTTP API clients from Python.",
     long_description=long_description,
     author="Ilja Everilä",
     author_email="saarni@gmail.com",
     url="https://github.com/everilae/furnish",
-    download_url="https://github.com/everilae/furnish/archive/v0.1.0.tar.gz",
+    download_url="https://github.com/everilae/furnish/archive/v0.1.1.tar.gz",
     packages=["furnish"],
     setup_requires=[ "pytest-runner" ],
     tests_require=[ "pytest" ],
